c16e958dab9cc1d08c898361ec5a647e9fca749b
Butch Landingin <butchland@gmail.com>
2022-12-04 14:14:49 +0800

    fix: Rename template file to stop quarto preview errors

--- initday.sh
+++ initday.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 day=$1
 mkdir -p exercises/day$day
-jinja -D day $day exercises/day-tmplt.qmd > exercises/day$day.qmd
+touch exercises/day$day/input.txt
+jinja -D day $day exercises/day-qmd.j2 > exercises/day$day.qmd
 jinja -D day $day butchland_aoc2022/code.j2 > butchland_aoc2022/day$day.py
 jinja -D day $day tests/testcode.j2 > tests/test_day$day.py
 echo "done init day $day"
