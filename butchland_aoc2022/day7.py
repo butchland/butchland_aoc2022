f3fff289e915f5e53d36653eda955479cd7da958
Butch Landingin <butchland@gmail.com>
2022-12-07 23:48:55 +0800

    feat: Add final notes for day 7

--- butchland_aoc2022/day7.py
+++ butchland_aoc2022/day7.py
@@ -1,7 +1,8 @@
-__all__ = ["parse_line"]
+__all__ = ["parse_line","generate_tokens", "process_commands"]
 
 # day7 
 import re
+from fastcore.all import *
 
 CD_CMD_PAT = r'\$ cd (.+)'
 LS_CMD_PAT = r'\$ ls'
@@ -26,3 +27,76 @@ def parse_line(line):
         fname = m.groups()[1]
         return ("size", int(fsize), fname)
     return None
+
+def generate_tokens(input):
+    for line in input:
+        token = parse_line(line)
+        yield token
+
+
+class Node:
+    def __init__(self,name, parent):
+        self.name = name
+        self.parent = parent
+        self.size = 0
+        self.children =  {}
+
+    def add_child(self, child):
+        self.children[child.name] = child
+
+    def get_descendant(self):
+        for child in self.children.values():
+            yield child.get_descendant()
+        yield self
+class TokenProcessor:
+    def __init__(self):
+        self.nodes = []
+        self.root = Node("/",None)
+        self.current = self.root
+        self.nodes.append(self.root)
+    
+    def process_token(self, token):
+        if token[0] == "cd":
+            if token[1] == "/":
+                self.current = self.root
+            elif token[1] == "..":
+                previous = self.current
+                current = self.current.parent
+                if current == None:
+                    raise ValueError("Could not go up higher, already at root")
+                current.size += previous.size
+                self.current = current
+            else:
+                if token[1] not in self.current.children:
+                    raise ValueError(f'cd {token[1]} not a valid directory')
+                current = self.current.children[token[1]]
+                self.current = current
+        elif token[0] == "size":
+            self.current.size += token[1] # add size
+        elif token[0] == "dir":
+            child = Node(token[1],self.current)
+            self.current.add_child(child)
+            self.nodes.append(child)
+    def return_current_to_root(self):
+        while self.current != self.root:
+            previous = self.current
+            current = self.current.parent
+            if current == None:
+                raise ValueError("Could not go up higher, already at root")
+            current.size += previous.size
+            self.current = current
+
+def process_commands(input):
+
+    token_processor = TokenProcessor()
+
+    for token in generate_tokens(input):
+        token_processor.process_token(token)
+
+    token_processor.return_current_to_root()
+
+    return token_processor.nodes
+
+
+        
+
--- tests/test_day7.py
+++ tests/test_day7.py
@@ -1,13 +1,57 @@
 import pytest
 from butchland_aoc2022.day7 import *
+import fastcore.all as fc
 
-@pytest.mark.parametrize(
-    "test_input, expected", [
+data = """$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k"""
+
+@pytest.fixture()
+def samples():
+    yield data.splitlines()
+
+expected_results =  [
         ("$ cd /", ("cd", "/")), 
         ("$ ls", ("ls",)), 
         ("dir a", ("dir", "a")), 
-        ("148448514 b.txt", ("size",148_448_514,"b.txt"))
-])
+        ("14848514 b.txt", ("size",14_848_514,"b.txt"))
+]
+@pytest.mark.parametrize(
+    "test_input, expected",expected_results)
 def test_parse_line(test_input, expected):
     assert parse_line(test_input) == expected
+
+
+def test_generate_tokens(samples):
+    token_gen = generate_tokens(samples)
+    for expected in fc.L(expected_results).itemgot(1):
+        token = next(token_gen)
+        assert token == expected
+
+def test_process_commands(samples):
+    nodes = process_commands(samples)
+    lnodes = fc.L(nodes)
+    tsize = lnodes.attrgot("size").filter(lambda o: o < 100_000).sum()
+    assert tsize == 95_437
+
 
