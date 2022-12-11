a66149fb6c295a52cf413008dcd818184876e04d
Butch Landingin <butchland@gmail.com>
2022-12-11 22:02:08 +0800

    fix: Correct implem of diagonal move

--- butchland_aoc2022/day9.py
+++ butchland_aoc2022/day9.py
@@ -1,4 +1,77 @@
-__all__ = []
+__all__ = ["process_moves","execute_move", "move_rope", "init_rope","Pos"]
 
 # day9 
+import math
 
+from collections import namedtuple
+
+Pos = namedtuple("Pos","r c") # position : row, column
+
+def init_rope(knots=2, init_pos=Pos(11,15)):
+    return tuple([init_pos] * knots)
+
+def move_head(h, dir):
+    if dir == "U":
+        return Pos(h.r-1, h.c)
+    elif dir == "D":
+        return Pos(h.r+1, h.c)
+    elif dir == "L":
+        return Pos(h.r, h.c-1)
+    else: # dir = "R"
+        return Pos(h.r, h.c+1)
+
+def is_adjacent(p1,p2):
+    return abs(p1.c - p2.c) < 2 and abs(p1.r - p2.r) < 2
+
+def is_diagonal(p1,p2):
+    return (p1.r != p2.r) and (p1.c != p2.c)
+
+def distance(p1,p2):
+    return Pos(p1.r-p2.r, p1.c-p2.c)
+
+def abs_distance(p1,p2):
+    dist = distance(p1,p2)
+    return Pos(abs(dist.r), abs(dist.c))
+
+def same_row(p1,p2):
+    return p1.r == p2.r
+
+def move_rope(rope,dir):
+    new_h = move_head(rope[0],dir)
+    new_rope = [new_h]
+    for i,k in enumerate(rope[1:]):
+        dist = distance(new_h, k)
+        if is_adjacent(new_h,k):
+            new_k = k # no movement
+        elif not is_diagonal(new_h, k):
+            if same_row(new_h, k):
+                new_k = Pos(k.r, k.c + int(math.copysign(1, dist.c)))
+            else: # same column
+                new_k = Pos(k.r + int(math.copysign(1,dist.r)), k.c)   
+        else: # is_diagonal(new_h, k):
+            new_k = Pos(k.r+int(math.copysign(1, dist.r)),
+                        k.c+int(math.copysign(1, dist.c)))
+        new_rope.append(new_k)
+        new_h = new_k
+    return tuple(new_rope)
+
+def parse_move(move):
+    dir,cnt = move.split(" ")
+    return (dir, int(cnt))
+
+def execute_move(rope, move):
+    dir, cnt = parse_move(move)
+    occupieds = set([rope[-1]])
+    for i in range(cnt):
+        rope = move_rope(rope, dir)
+        occupieds.add(rope[-1])
+    return rope, occupieds
+
+def process_moves(moves, rope=None, knots=2,init_pos=Pos(11,15)):
+    if rope is None:
+        rope = init_rope(knots=knots, init_pos=init_pos)
+    all_occupieds = set([rope[-1]])
+    for move in moves:
+        rope, occupieds = execute_move(rope, move)
+        all_occupieds.update(occupieds)
+    return rope,all_occupieds
--- tests/test_day9.py
+++ tests/test_day9.py
@@ -1,4 +1,169 @@
 import pytest
 from butchland_aoc2022.day9 import *
+sm_input = """R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"""
 
+md_input = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
+
+@pytest.fixture()
+def sm_samples():
+    yield sm_input.splitlines()
+
+@pytest.fixture()
+def md_samples():
+    yield md_input.splitlines()
+
+def test_process_moves(md_samples):
+    rope = init_rope(knots=10, init_pos=Pos(16,12))
+    final_rope, all_occupieds = process_moves(md_samples,rope=rope)
+    assert len(final_rope) == 10
+    assert len(all_occupieds) == 36
+    assert final_rope == tuple([Pos(1,1),Pos(2,1),Pos(3,1),Pos(4,1),Pos(5,1),
+                                    Pos(6,1),Pos(7,1),Pos(8,1),Pos(9,1),Pos(10,1)])
+
+    assert all_occupieds.issuperset(
+            set([Pos(16,12),Pos(15,13),Pos(14,14),Pos(13,13),])
+            )
+
+
+def test_process_moves2(sm_samples):
+    rope = init_rope(knots=2, init_pos=Pos(5,1))
+    final_rope, all_occupieds = process_moves(sm_samples,rope=rope)
+    assert len(final_rope) == 2
+    assert len(all_occupieds) == 13
+    assert final_rope == tuple([Pos(3,3),Pos(3,2)])
+    assert all_occupieds.issuperset(
+            set([Pos(5,1),Pos(5,2),Pos(5,3),Pos(5,4),
+                 Pos(4,5),
+                 Pos(3,2),Pos(3,3),Pos(3,4),Pos(3,5),
+                 Pos(2,4),Pos(2,5),
+                 Pos(1,3),Pos(1,4)])
+            )
+
+def test_process_moves3(sm_samples):
+    rope = init_rope(knots=10, init_pos=Pos(5,1))
+    final_rope, all_occupieds = process_moves(sm_samples,rope=rope)
+    assert len(final_rope) == 10
+    assert len(all_occupieds) == 1
+    assert final_rope == tuple([Pos(3,3),Pos(3,2),Pos(3,3),Pos(3,4),Pos(3,3),
+                                Pos(4,2),Pos(5,1),Pos(5,1),Pos(5,1),Pos(5,1)])
+    assert all_occupieds.issuperset(
+            set([Pos(5,1)])
+            )
+
+def test_process_moves3a(sm_samples):
+    rope = init_rope(knots=10, init_pos=Pos(5,1))
+    final_rope, all_occupieds = process_moves(sm_samples[:-1],rope=rope)
+    assert len(final_rope) == 10
+    assert len(all_occupieds) == 1
+    assert final_rope == tuple([Pos(3,1),Pos(3,2),Pos(3,3),Pos(3,4),Pos(3,3),
+                                Pos(4,2), # wrong = Pos(3,2)
+                                Pos(5,1),Pos(5,1),Pos(5,1),Pos(5,1)])
+    assert all_occupieds.issuperset(
+            set([Pos(5,1)])
+            )
+
+
+def test_process_moves3b(sm_samples):
+    rope = init_rope(knots=10, init_pos=Pos(5,1))
+    final_rope, all_occupieds = process_moves(sm_samples[:1],rope=rope)
+    assert len(final_rope) == 10
+    assert len(all_occupieds) == 1
+    assert final_rope == tuple([Pos(5,5),Pos(5,4),Pos(5,3),Pos(5,2),Pos(5,1),
+                                Pos(5,1), 
+                                Pos(5,1),Pos(5,1),Pos(5,1),Pos(5,1)])
+    assert all_occupieds.issuperset(
+            set([Pos(5,1)])
+            )
+    
+def test_process_moves3b1():
+    rope = tuple([Pos(5,5),Pos(5,4),Pos(5,3),Pos(5,2),Pos(5,1),
+                                Pos(5,1), 
+                                Pos(5,1),Pos(5,1),Pos(5,1),Pos(5,1)])
+    final_rope, all_occupieds = process_moves(["U 1"],rope=rope)
+    assert len(final_rope) == 10
+    assert len(all_occupieds) == 1
+    assert final_rope == tuple([Pos(4,5),Pos(5,4),Pos(5,3),Pos(5,2),Pos(5,1),
+                                Pos(5,1), 
+                                Pos(5,1),Pos(5,1),Pos(5,1),Pos(5,1)])
+    assert all_occupieds.issuperset(
+            set([Pos(5,1)])
+            )
+
+def test_process_moves3b2():
+    rope = tuple([Pos(4,5),Pos(5,4),Pos(5,3),Pos(5,2),Pos(5,1),
+                                Pos(5,1), 
+                                Pos(5,1),Pos(5,1),Pos(5,1),Pos(5,1)])
+    final_rope, all_occupieds = process_moves(["U 1"],rope=rope)
+    assert len(final_rope) == 10
+    assert len(all_occupieds) == 1
+    assert final_rope == tuple([Pos(3,5),Pos(4,5),Pos(4,4),Pos(4,3),Pos(4,2),
+                                Pos(5,1), 
+                                Pos(5,1),Pos(5,1),Pos(5,1),Pos(5,1)])
+    assert all_occupieds.issuperset(
+            set([Pos(5,1)])
+            )
+def test_process_moves3b3():
+    rope = tuple([Pos(3,5),Pos(4,5),Pos(4,4),Pos(4,3),Pos(4,2),
+                                Pos(5,1), 
+                                Pos(5,1),Pos(5,1),Pos(5,1),Pos(5,1)])
+    final_rope, all_occupieds = process_moves(["U 1"],rope=rope)
+    assert len(final_rope) == 10
+    assert len(all_occupieds) == 1
+    assert final_rope == tuple([Pos(2,5),Pos(3,5),Pos(4,4),Pos(4,3),Pos(4,2),
+                                Pos(5,1), 
+                                Pos(5,1),Pos(5,1),Pos(5,1),Pos(5,1)])
+    assert all_occupieds.issuperset(
+            set([Pos(5,1)])
+            )
+    
+def test_move_rope3b4():
+    rope =  tuple([Pos(2,5),Pos(3,5),Pos(4,4),Pos(4,3),Pos(4,2),
+                                Pos(5,1), 
+                                Pos(5,1),Pos(5,1),Pos(5,1),Pos(5,1)])
+    final_rope = move_rope(rope, "U")
+    assert len(final_rope) == 10
+    assert final_rope == tuple([Pos(1,5),Pos(2,5),Pos(3,5),Pos(3,4),Pos(3,3),
+                                Pos(4,2), # wrong Pos(3,2)
+                                Pos(5,1),Pos(5,1),Pos(5,1),Pos(5,1)])
+
+
+def test_process_moves3b4():
+    rope =  tuple([Pos(2,5),Pos(3,5),Pos(4,4),Pos(4,3),Pos(4,2),
+                                Pos(5,1), 
+                                Pos(5,1),Pos(5,1),Pos(5,1),Pos(5,1)])
+    final_rope, all_occupieds = process_moves(["U 1"],rope=rope)
+    assert len(final_rope) == 10
+    assert len(all_occupieds) == 1
+    assert final_rope == tuple([Pos(1,5),Pos(2,5),Pos(3,5),Pos(3,4),Pos(3,3),
+                                Pos(4,2), 
+                                Pos(5,1),Pos(5,1),Pos(5,1),Pos(5,1)])
+    assert all_occupieds.issuperset(
+            set([Pos(5,1)])
+            )
+
+def test_process_moves3c(sm_samples):
+    rope = init_rope(knots=10, init_pos=Pos(5,1))
+    final_rope, all_occupieds = process_moves(sm_samples[:2],rope=rope)
+    assert len(final_rope) == 10
+    assert len(all_occupieds) == 1
+    assert final_rope == tuple([Pos(1,5),Pos(2,5),Pos(3,5),Pos(3,4),Pos(3,3),
+                                Pos(4,2), 
+                                Pos(5,1),Pos(5,1),Pos(5,1),Pos(5,1)])
+    assert all_occupieds.issuperset(
+            set([Pos(5,1)])
+            )
 
